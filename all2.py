10feaa4df02b09e11c2fd34747a99296c5885de6
rajan <rajansah0020@gmail.com>
2024-05-18 18:45:29 +0200

    Update all2.py

--- all2.py
+++ all2.py
@@ -149,23 +149,23 @@ else:  # English to German
             st.write('Please enter both the English word and its German translation.')
 
 
-
-if st.button('Next'):
-    st.session_state.current_index = (st.session_state.current_index + 1) % len(dictionary_df)
-    st.session_state.flipped = False
-
-if st.button('Previous'):
-    st.session_state.current_index = (st.session_state.current_index - 1) % len(dictionary_df)
-    st.session_state.flipped = False
-
-
 # Display the current flashcard and pronounce the word
 current_word = render_flashcard(st.session_state.current_index, st.session_state.flipped)
 
+# Flashcard navigation buttons
+col1, col2, col3 = st.columns(3)
+with col1:
+    if st.button('Previous'):
+        st.session_state.current_index = (st.session_state.current_index - 1) % len(dictionary_df)
+        st.session_state.flipped = False
+with col2:
+    if st.button('Flip'):
+        st.session_state.flipped = not st.session_state.flipped
+with col3:
+    if st.button('Next'):
+        st.session_state.current_index = (st.session_state.current_index + 1) % len(dictionary_df)
+        st.session_state.flipped = False
 
-# Flashcard navigation
-if st.button('Flip'):
-    st.session_state.flipped = not st.session_state.flipped
     
 #, rate=150
 def pronounce_word(word):
