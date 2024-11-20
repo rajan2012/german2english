9ad450a0c2c26fb3c9ea93736830d5a5f59557bf
rajan <rajansah0020@gmail.com>
2024-11-20 20:38:00 +0100

    Update eng2germ.py

--- eng2germ.py
+++ eng2germ.py
@@ -26,4 +26,28 @@ def english_to_german_translation():
             english_word = ''
 
 
+import streamlit as st
+from googletrans import Translator
+from gtts import gTTS
+
+def german_to_english_translation():
+    # Initialize Google Translator
+    translator = Translator()
+
+    # Input for German word
+    german_word = st.text_input('Enter a German word:', '')
+
+    # Translate German to English using Google Translate API
+    english_word = ''
+    if german_word:
+        english_word = translator.translate(german_word, dest='en', src='de').text
+        st.write(f"Translation in English: {english_word}")
+
+        # Option to pronounce the English word
+        if st.button('Pronounce 4'):
+            tts = gTTS(text=english_word, lang='en')
+            tts.save("translated_word_temp.mp3")
+            audio_file = open("translated_word_temp.mp3", "rb")
+            audio_bytes = audio_file.read()
+            st.audio(audio_bytes, format='audio/mp3', start_time=0, autoplay=True)
 
