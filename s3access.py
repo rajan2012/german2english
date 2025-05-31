9d11d420bca821e8bd07eb20043af37e05e00d4e
rajan <rajansah0020@gmail.com>
2025-05-31 11:53:09 +0200

    Update s3access.py

--- s3access.py
+++ s3access.py
@@ -80,19 +80,7 @@ images_path = r"C:\Users\User\Documents\german\image"
 #last_modified = 0  # Set initial last modified timestamp
 dictionary_df = load_data_s3(bucket, filename)
 
-# Input box for German word
-german_word_s = st.text_input("Enter a German word for search:")
 
-# If input is given, search for it
-if german_word_s:
-    # Case-insensitive search
-    match = dictionary_df[dictionary_df['German'].str.lower() == german_word_s.lower()]
-
-    if not match.empty:
-        english_translation = match.iloc[0]['English']
-        st.success(f"**English:** {english_translation}")
-    else:
-        st.warning("Word not found in the dictionary.")
 
 # Initialize session state for the current index and flip state
 if 'current_index' not in st.session_state:
@@ -145,6 +133,20 @@ def pronounce_word(word, rate=150):
 
 # Streamlit app
 st.title('Translation Dictionary')
+# Input box for German word
+german_word_s = st.text_input("Enter a German word for search:")
+
+# If input is given, search for it
+if german_word_s:
+    # Case-insensitive search
+    match = dictionary_df[dictionary_df['German'].str.lower() == german_word_s.lower()]
+
+    if not match.empty:
+        english_translation = match.iloc[0]['English']
+        st.success(f"**English:** {english_translation}")
+    else:
+        st.warning("Word not found in the dictionary.")
+
 # Run the method
 english_to_german_translation()
 german_to_english_translation()
