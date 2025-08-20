f8eaba870b90c3556762bfad19108e4faa38814a
rajan <rajansah0020@gmail.com>
2025-08-20 22:21:03 +0200

    updated code

--- s3access.py
+++ s3access.py
@@ -154,7 +154,7 @@ def update_dictionary_auto_eng2de(new_words, dictionary_df):
     #st.write("new_words", len(new_words))
 
     # Raise exception if sentence is mostly German
-    if german_count >= 5 or german_count == len(new_words):
+    if german_count >= 50 or german_count == len(new_words):
         raise ValueError("Please enter an English sentence, not German.")
 
     for word in new_words:
